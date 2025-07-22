e5b588b2354422e78669df13177ca64dd1d36765
Randrianasolo Tambatra Hasiniavo Roger <randrianasolotambatrahasiniavo@gmail.com>
2025-07-22 16:18:19 +0200

    convention collective

--- src/shared/components/Sidebar/Sidebar.tsx
+++ src/shared/components/Sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FiltersPanel, FiltersPanelProps } from "./FiltersPanel";
 
 export const Sidebar: React.FC<FiltersPanelProps> = (props) => (
-  <div className="w-80 bg-white border-r border-gray-200 h-screen overflow-y-auto">
+  <div className="w-96 bg-white border-r border-gray-200 h-screen overflow-y-auto">
     <FiltersPanel {...props} />
   </div>
 );
